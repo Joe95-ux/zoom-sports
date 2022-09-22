a5ab24131b74a8679773b8815e63169ba2b11f56
joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2022-09-22 15:40:44 +0100

    add auth to ckeditor token route

--- routes/ckeditorurl.js
+++ routes/ckeditorurl.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require( 'jsonwebtoken' );
+const jwt = require("jsonwebtoken");
+const { ensureAuth } = require("../middleware/auth");
 
 const accessKey = process.env.CKEDITOR_ACCESS_KEY;
 const environmentId = process.env.CKEDITOR_ENV_ID;
 
+router.get("/ckeditor", ensureAuth, (req, res) => {
+  const payload = {
+    aud: environmentId,
+    sub: "#80105",
+    user: {
+      email: "agborayuk099@gmail.com",
+      name: "ayuk"
+    },
+    auth: {
+      ckbox: {
+        role: "admin"
+      }
+    }
+  };
 
+  const result = jwt.sign(payload, accessKey, {
+    algorithm: "HS256",
+    expiresIn: "24h"
+  });
 
-router.get( '/ckeditor', ( req, res ) => {
-    const payload = {
-        aud: environmentId,
-        sub: '#80105',
-        user: {
-            email: 'agborayuk099@gmail.com',
-            name: 'ayuk'
-        },
-        "auth": {
-            "ckbox": {
-                "role": "admin"
-            }
-        }
-        
-    };
-
-    const result = jwt.sign( payload, accessKey, { algorithm: 'HS256', expiresIn: '24h' } );
-
-    res.send( result );
-} );
+  res.send(result);
+});
 
 module.exports = router;
