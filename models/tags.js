13997e59b02f3de45cebcd7752050dd8a37ea507
joe95-ux <ogorktabi@gmail.com>
2023-06-07 23:56:43 +0100

    add tags schema

---
+++ models/tags.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const slugify = require('slugify');
+
+const tagSchema = new mongoose.Schema({
+  name: String
+});
+
+
+
+module.exports = mongoose.model("Tag", tagSchema);
